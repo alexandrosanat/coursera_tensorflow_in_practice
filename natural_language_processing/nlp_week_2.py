3af30f947f6d3d5dcae6952c5fc166e77382bde7
alexandrosanat <alexandros_anat@hotmail.com>
2020-07-04 20:31:21 +0100

    Develop model

--- natural_language_processing/nlp_week_2.py
+++ natural_language_processing/nlp_week_2.py
@@ -1,3 +1,102 @@
 import tensorflow as tf
+import tensorflow_datasets as tfds
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow_datasets.core.utils import gcs_utils
+import numpy as np
+import io
 
-print(tf.__version__)
+gcs_utils.gcs_dataset_info_files = lambda *args, **kwargs: None
+gcs_utils.is_dataset_on_gcs = lambda *args, **kwargs: False
+
+# Import IMDB dataset
+imdb, _ = tfds.load("imdb_reviews", with_info=True, as_supervised=True)
+# Split test/training sets
+train_data, test_data = imdb["train"], imdb["test"]
+
+training_sentences = []
+training_labels = []
+
+testing_sentences = []
+testing_labels = []
+
+# Get training data from tensors
+for s, l in train_data:
+    training_sentences.append(str(s.numpy()))
+    training_labels.append(l.numpy())
+
+# Get test data from tensors
+for s, l in test_data:
+    testing_sentences.append(str(s.numpy()))
+    testing_labels.append(l.numpy())
+
+# Convert arrays to numpy arrays
+training_labels_final = np.array(training_labels)
+testing_labels_final = np.array(testing_labels)
+
+# Specify parameters
+vocab_size = 10000
+embedding_size = 16
+max_length = 120
+trunc_type = "post"
+oov_tok = "<OOV>"
+
+# Create a Tokenizer
+tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+tokenizer.fit_on_texts(training_sentences)
+
+# Create a word index
+word_index = tokenizer.word_index
+training_sequences = tokenizer.texts_to_sequences(training_sentences)
+training_padded = pad_sequences(training_sequences, maxlen=max_length, truncating=trunc_type)
+# Use same tokenizer in test data - a lot more OOVs might be created
+testing_sentences = tokenizer.texts_to_sequences(testing_sentences)
+testing_padded = pad_sequences(testing_sentences, maxlen=max_length, truncating=trunc_type)
+
+# Reverse word index
+reverse_word_index = dict([(value, key) for key, value in word_index.items()])
+
+
+def decode_review(text):
+    return " ".join([reverse_word_index.get(i, "?") for i in text])
+
+
+# Examine an example
+print(decode_review(training_padded[1]))
+print(training_sentences[1])
+
+# Define a Sequential Neural Network
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_size, input_length=max_length),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(6, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')])
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.summary()
+
+# Fit model
+num_epochs = 10
+model.fit(training_padded, training_labels_final, epochs=num_epochs,
+          validation_data=(testing_padded, testing_labels_final))
+
+# Prepare data for embeddings projector
+
+e = model.layers[0] # Take output of embedding (layer 0)
+weights = e.get_weights()[0]
+print(weights.shape)  # 10000 words, 16 dimensions
+
+out_v = io.open('vecs.tsv', 'w', encoding='utf-8')
+out_m = io.open('meta.tsv', 'w', encoding='utf-8')
+for word_num in range(1, vocab_size):
+    word = reverse_word_index[word_num]
+    embeddings = weights[word_num]
+    out_m.write(word + "\n")
+    out_v.write('\t'.join([str(x) for x in embeddings]) + "\n")
+out_v.close()
+out_m.close()
+
+sentence = "I really think this is amazing. honest."
+sequence = tokenizer.texts_to_sequences([sentence])
+print(sequence)
+
